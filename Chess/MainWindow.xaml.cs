da88dbe4412c1591d42555e8f5bf4517dcb9c4a6
angrydogeee@gmail.com <angrydogeee@gmail.com>
2021-12-17 00:52:47 +0700

    enum

--- Chess/MainWindow.xaml.cs
+++ Chess/MainWindow.xaml.cs
@@ -9,10 +9,8 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using static System.Math;
 
 
 namespace Chess
@@ -30,56 +28,69 @@ namespace Chess
 
             Start();
         }
+        public enum ChessSide
+        {
+            White = 0,
+            Black,
+            NoSide
+        }
+        public enum ChessType
+        {
+            Notype = 0,
+            Pawn,
+            Rook,
+            Knight,
+            Bishop,
+            Quenn,
+            King
+        }
 
         public class NewButton : Button
         {
-            public int Side { get; set; }
-            public int Turn = 0;
-            public int Type { get; set; }
+            public ChessSide Side { get; set; }
+            public bool FirstTurn = true;
+            public ChessType Type { get; set; }
             public int X { get; set; }
             public int Y { get; set; }
-            public bool Potential(int x2, int y2, int s)
+            public bool Potential(int x2, int y2, ChessSide s)
             {
                 switch (Type)
                 {
-                    case 1: //pawn
+                    case ChessType.Pawn:
                         {
                             int temp = 1;
                             if (Side == 0)
                                 temp = -1;
-                            if (Turn == 0)
+                            if (FirstTurn)
                             {
-                                if ((x2 == X + temp || (x2 == X + 2 * temp && buttonMap[X + temp, Y].Type == 0)) && Y == y2 && s == 2 || (s != Side && s != 2 && x2 == X + temp && (Y == y2 + 1 || Y == y2 - 1)))
+                                if ((x2 == X + temp || (x2 == X + 2 * temp && buttonMap[X + temp, Y].Type == 0)) && Y == y2 && s == ChessSide.NoSide || (s != Side && s != ChessSide.NoSide && x2 == X + temp && (Y == y2 + 1 || Y == y2 - 1)))
                                     return true;
                                 else
                                     return false;
                             }
                             else
                             {
-                                if ((x2 == X + temp && Y == y2 && s == 2) || (s != Side && s != 2 && x2 == X + temp && (Y == y2 + 1 || Y == y2 - 1)))
+                                if ((x2 == X + temp && Y == y2 && s == ChessSide.NoSide) || (s != Side && s != ChessSide.NoSide && x2 == X + temp && (Y == y2 + 1 || Y == y2 - 1)))
                                     return true;
                                 else
                                     return false;
                             }
                         }
-                    case (2): //rook
+                    case ChessType.Rook:
                         return HorizontalVertical(X, Y, x2, y2, Side, true);
-                    case (3): //knight
+                    case ChessType.Knight:
                         {
-                            if ((Math.Abs(X - x2) == 2 && Math.Abs(Y - y2) == 1) || (Math.Abs(X - x2) == 1 && Math.Abs(Y - y2) == 2))
+                            if ((Abs(X - x2) == 2 && Abs(Y - y2) == 1) || (Abs(X - x2) == 1 && Abs(Y - y2) == 2))
                                 return true;
                             else
                                 return false;
                         }
-                    case (4): //bishop
+                    case ChessType.Bishop:
                         return Diagonal(X, Y, x2, y2, Side, true);
-                    case (5): //queen
+                    case ChessType.Quenn:
                         return HorizontalVertical(X, Y, x2, y2, Side, true) || Diagonal(X, Y, x2, y2, Side, true);
-                    case (6): //king
-                        {
+                    case ChessType.King:
                             return HorizontalVertical(X, Y, x2, y2, Side, false) || Diagonal(X, Y, x2, y2, Side, false);
-                        }
-
                     default:
                         return false;
                 }
@@ -87,91 +98,91 @@ namespace Chess
             public NewButton() { }
             public NewButton(int Side, int Type, int X, int Y)
             {
-                this.Side = Side;
-                this.Type = Type;
+                this.Side = (ChessSide)Side;
+                this.Type = (ChessType)Type;
                 this.X = X;
                 this.Y = Y;
                 string currentDir = Environment.CurrentDirectory;
-                switch (Side, Type)
+                switch (this.Side, this.Type)
                 {
-                    case (1, 1):
+                    case (ChessSide.Black, ChessType.Pawn):
                         {
                             Image img = new Image();
                             img.Source = new BitmapImage(new Uri(currentDir + @"\Fig\pawnBlack.png"));
                             this.Content = img;
                             break;
                         }
-                    case (1, 2):
+                    case (ChessSide.Black, ChessType.Rook):
                         {
                             Image img = new Image();
                             img.Source = new BitmapImage(new Uri(currentDir + @"\Fig\rookBlack.png"));
                             this.Content = img;
                             break;
                         }
-                    case (1, 3):
+                    case (ChessSide.Black, ChessType.Knight):
                         {
                             Image img = new Image();
                             img.Source = new BitmapImage(new Uri(currentDir + @"\Fig\knightBlack.png"));
                             this.Content = img;
                             break;
                         }
-                    case (1, 4):
+                    case (ChessSide.Black, ChessType.Bishop):
                         {
                             Image img = new Image();
                             img.Source = new BitmapImage(new Uri(currentDir + @"\Fig\bishopBlack.png"));
                             this.Content = img;
                             break;
                         }
-                    case (1, 5):
+                    case (ChessSide.Black, ChessType.Quenn):
                         {
                             Image img = new Image();
                             img.Source = new BitmapImage(new Uri(currentDir + @"\Fig\queenBlack.png"));
                             this.Content = img;
                             break;
                         }
-                    case (1, 6):
+                    case (ChessSide.Black, ChessType.King):
                         {
                             Image img = new Image();
                             img.Source = new BitmapImage(new Uri(currentDir + @"\Fig\kingBlack.png"));
                             this.Content = img;
                             break;
                         }
-                    case (0, 1):
+                    case (ChessSide.White, ChessType.Pawn):
                         {
                             Image img = new Image();
                             img.Source = new BitmapImage(new Uri(currentDir + @"\Fig\pawnWhite.png"));
                             this.Content = img;
                             break;
                         }
-                    case (0, 2):
+                    case (ChessSide.White, ChessType.Rook):
                         {
                             Image img = new Image();
                             img.Source = new BitmapImage(new Uri(currentDir + @"\Fig\rookWhite.png"));
                             this.Content = img;
                             break;
                         }
-                    case (0, 3):
+                    case (ChessSide.White, ChessType.Knight):
                         {
                             Image img = new Image();
                             img.Source = new BitmapImage(new Uri(currentDir + @"\Fig\knightWhite.png"));
                             this.Content = img;
                             break;
                         }
-                    case (0, 4):
+                    case (ChessSide.White, ChessType.Bishop):
                         {
                             Image img = new Image();
                             img.Source = new BitmapImage(new Uri(currentDir + @"\Fig\bishopWhite.png"));
                             this.Content = img;
                             break;
                         }
-                    case (0, 5):
+                    case (ChessSide.White, ChessType.Quenn):
                         {
                             Image img = new Image();
                             img.Source = new BitmapImage(new Uri(currentDir + @"\Fig\queenWhite.png"));
                             this.Content = img;
                             break;
                         }
-                    case (0, 6):
+                    case (ChessSide.White, ChessType.King):
                         {
                             Image img = new Image();
                             img.Source = new BitmapImage(new Uri(currentDir + @"\Fig\kingWhite.png"));
@@ -202,9 +213,9 @@ namespace Chess
 
         public bool themaLight = true;
 
-        public string[] sideArray = new string[2] { "White", "Black" };
+        //public string[] sideArray = new string[2] { "White", "Black" };
 
-        string[] alpha = { "A", "B", "C", "D", "E", "F", "G", "H" };
+        //string[] alpha = { "A", "B", "C", "D", "E", "F", "G", "H" };
 
         public void CreateMap()
         {
@@ -217,7 +228,8 @@ namespace Chess
                 chessNumber1.Children.Add(textBlock);
 
                 TextBlock textBlock1 = new TextBlock();
-                textBlock1.Text = alpha[i];
+                //textBlock1.Text = alpha[i];
+                textBlock1.Text = ((char)(65 + i)).ToString();
                 textBlock1.TextAlignment = TextAlignment.Center;
                 textBlock.VerticalAlignment = VerticalAlignment.Center;
                 chessLetter1.Children.Add(textBlock1);
@@ -232,7 +244,8 @@ namespace Chess
                 chessNumber2.Children.Add(textBlock);
 
                 TextBlock textBlock1 = new TextBlock();
-                textBlock1.Text = alpha[i].ToString();
+                //textBlock1.Text = alpha[i].ToString();
+                textBlock1.Text = ((char)(65 + i)).ToString();
                 textBlock1.TextAlignment = TextAlignment.Center;
                 textBlock.VerticalAlignment = VerticalAlignment.Center;
                 chessLetter2.Children.Add(textBlock1);
@@ -243,55 +256,51 @@ namespace Chess
         {
             NewButton pressedButton = (NewButton)sender;
 
-            if (pressedButton.Content != null && pressedButton.Side == turnNumber % 2)
+            if (pressedButton.Content != null && pressedButton.Side == (ChessSide)(turnNumber % 2))
             {
                 if (previousButton != null)
                 {
                     previousButton.Effect = null;
                     previousButton.BorderBrush = Brushes.Black;
                 }
-                BlurEffect blur = new BlurEffect { Radius = 1 };
-                pressedButton.Effect = blur;
-                pressedButton.BorderBrush = Brushes.Red;
+                pressedButton.BorderBrush = Brushes.Red;                    //***************какой-то эффект нужен более интересный для выделения
+                //pressedButton.Foreground = Brushes.Red;
                 previousButton = pressedButton;
             }
-            else if (previousButton != null && (pressedButton.Content == null || pressedButton.Content != null))
-                if (previousButton.Potential(pressedButton.X, pressedButton.Y, pressedButton.Side))
-                    Turn(previousButton, pressedButton);
+            else if (previousButton != null && (pressedButton.Content == null || pressedButton.Content != null) && previousButton.Potential(pressedButton.X, pressedButton.Y, pressedButton.Side))
+                Turn(previousButton, pressedButton);
         }
 
         public void Turn(NewButton button1, NewButton button2)
         {
             TextTurn(button1, button2);
-            if (button2.Type == 6)
+            if (button2.Type == ChessType.King)
             {
-                MessageBox.Show(sideArray[turnNumber % 2] + "Win");
+                MessageBox.Show((ChessSide)(turnNumber % 2) + " Win");  
                 DisableButtons();
             }
             button2.Content = button1.Content;
             button2.Type = button1.Type;
             button2.Side = button1.Side;
-            button2.Turn = button1.Turn;
-            if (button2.Turn == 0)
-                button2.Turn = 1;
+            button2.FirstTurn = false;
             button1.Content = null;
-            button1.Type = 0;
-            button1.Side = 2;
+            button1.Type = ChessType.Notype;
+            button1.Side = ChessSide.NoSide;
 
             turnNumber++;
             TurnTextBox.Text = (turnNumber + 1).ToString();
-            SideTextBox.Text = sideArray[turnNumber % 2];
+            SideTextBox.Text = ((ChessSide)(turnNumber % 2)).ToString();
             Brush[] b = { Brushes.White, Brushes.Black };
             FunImage.Background = b[turnNumber % 2];
         }
 
-        public static bool HorizontalVertical(int x1, int y1, int x2, int y2, int s, bool check)
+        public static bool HorizontalVertical(int x1, int y1, int x2, int y2, ChessSide s, bool check)
         {
             if (x1 == x2 || y1 == y2)
             {
                 if (x1 == x2)
                 {
-                    int signY = Math.Sign(y1 - y2), y = y1 - signY;
+                    int signY = Sign(y1 - y2), y = y1 - signY;
                     if (check)
                         while (y != y2 && buttonMap[x1, y].Type == 0)
                             y -= signY;
@@ -302,7 +311,7 @@ namespace Chess
                 }
                 else
                 {
-                    int signX = Math.Sign(x1 - x2), x = x1 - signX;
+                    int signX = Sign(x1 - x2), x = x1 - signX;
                     if (check)
                         while (x != x2 && buttonMap[x, y1].Type == 0)
                             x -= signX;
@@ -316,12 +325,12 @@ namespace Chess
                 return false;
         }
 
-        public static bool Diagonal(int x1, int y1, int x2, int y2, int s, bool check)
+        public static bool Diagonal(int x1, int y1, int x2, int y2, ChessSide s, bool check)
         {
-            if (Math.Abs(x1 - x2) == Math.Abs(y1 - y2))
+            if (Abs(x1 - x2) == Abs(y1 - y2))
             {
-                int signX = Math.Sign(x1 - x2),
-                signY = Math.Sign(y1 - y2),
+                int signX = Sign(x1 - x2),
+                signY = Sign(y1 - y2),
                 x = x1 - signX, y = y1 - signY;
                 if (check)
                     while (x != x2 && y != y2 && buttonMap[x, y].Type == 0)
@@ -358,28 +367,22 @@ namespace Chess
             previousButton = null;
             turnNumber = 0;
             TextBox1.Text = null;
-            TurnTextBox.Text = (turnNumber + 1).ToString();
-            SideTextBox.Text = sideArray[0];
+            TurnTextBox.Text = "1";
+            SideTextBox.Text = "White";
             FunImage.Background = Brushes.White;
             for (int i = 0; i < 8; i++)
                 for (int j = 0; j < 8; j++)
                 {
                     buttonMap[i, j] = new NewButton(map[i, j] / 10, map[i, j] % 10, i, j);
-
                     buttonMap[i, j].BorderBrush = Brushes.Black;
-
                     chessBoard.Children.Add(buttonMap[i, j]);
                 }
 
-            themaChange(themaLight);
+            ThemaChange(themaLight);
 
             foreach (UIElement c in chessBoard.Children)
-            {
                 if (c is Button)
-                {
                     ((Button)c).Click += Pressed;
-                }
-            }
         }
 
         public void LightThema(object sender, RoutedEventArgs e)
@@ -387,7 +390,7 @@ namespace Chess
             if (!themaLight)
             {
                 themaLight = true;
-                themaChange(themaLight);
+                ThemaChange(themaLight);
             }
         }
 
@@ -396,11 +399,11 @@ namespace Chess
             if (themaLight)
             {
                 themaLight = false;
-                themaChange(themaLight);
+                ThemaChange(themaLight);
             }
         }
 
-        public void themaChange(bool temp)
+        public void ThemaChange(bool temp)
         {
             Brush br1, br2;
             if (temp)
@@ -430,7 +433,7 @@ namespace Chess
         }
 
 
-        public void TextTurn(NewButton button1, NewButton button2)
+        public void TextTurn(NewButton button1, NewButton button2)   //*************тут подумать с символами
         {
             int kostyl = 1;
             string[] fig =
@@ -449,12 +452,14 @@ namespace Chess
                 "\u265b", //blackQueen
                 "\u265a", //blackKing
             };
-            if (button2.Side == 2)
+            if (button2.Side == ChessSide.NoSide)
                 kostyl = 0;
-            TextBox1.Text += "Turn: " + (turnNumber + 1) + "\t" + fig[button1.Type + 6 * button1.Side]
-                + " " + alpha[button1.Y].ToLower() + (8 - button1.X)
-                + " " + alpha[button2.Y].ToLower() + (8 - button2.X)
-                + " " + fig[button2.Type + 6 * button2.Side * kostyl] + "\n";
+            //TextBox1.Text += "Turn: " + (turnNumber + 1) + "\t" + fig[button1.Type + 6 * button1.Side]
+            //        + " " + ((char)(65 + button1.Y)).ToString().ToLower() + (8 - button1.X)
+            //        + " " + alpha[button2.Y].ToLower() + (8 - button2.X)
+            //        + " " + fig[button2.Type + 6 * (int)temp * kostyl] + "\n";
+            /*string temp = button1.Side.ToString();
+            int n = button1.Side;*/
         }
     }
 }
